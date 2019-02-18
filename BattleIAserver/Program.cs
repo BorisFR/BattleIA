9756775ccc7cd84cace7b62be06ec6fdecf73373
Boris <sfardoux@hotmail.com>
2019-02-18 08:00:43 +0100

    Add new server console

---
+++ BattleIAserver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BattleIAserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var ConsOut = Console.Out;  //Save the reference to the old out value (The terminal)
+            //Console.SetOut(new StreamWriter(Stream.Null)); //Remove console output
+
+            //Console.WriteLine(Directory.GetCurrentDirectory());
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Path.Combine(Directory.GetCurrentDirectory(), "WebPages"))
+            .UseStartup<Startup>()
+            .Build();                     //Modify the building per your needs
+
+            host.Start();                     //Start server non-blocking
+
+            //Console.SetOut(ConsOut);          //Restore output
+
+            //Regular console code
+            while (true)
+            {
+                Console.WriteLine(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ BattleIAserver/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.WebSockets;
+using System.Threading;
+
+namespace BattleIAserver
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) { }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/websockets?view=aspnetcore-2.2
+
+            // parametres pour réception des websocket
+            var webSocketOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(30),
+                ReceiveBufferSize = 4 * 1024
+            };
+            //webSocketOptions.AllowedOrigins.Add("http://ly0500");
+            //webSocketOptions.AllowedOrigins.Add("wss://ly0500");
+            //webSocketOptions.AllowedOrigins.Add("file://");
+
+            app.UseWebSockets(webSocketOptions);
+
+            app.Use(async (context, next) =>
+            {
+                // ouverture d'une websocket, un nouveau client se connecte
+                if (context.Request.Path == "/ia")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        // on l'ajoute à notre jeu !
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        //await Echo(context, webSocket);
+                        await MainGame.AddClient(webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    if (context.Request.Path == "/display")
+                    {
+                        if (context.WebSockets.IsWebSocketRequest)
+                        {
+                            // on l'ajoute à notre jeu !
+                            WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                            await MainGame.AddViewer(webSocket);
+                        }
+                        else
+                        {
+                            context.Response.StatusCode = 400;
+                        }
+                    }
+                    else
+                    {
+                        await next();
+                    }
+                }
+            });
+        }
+
+    }
+}
